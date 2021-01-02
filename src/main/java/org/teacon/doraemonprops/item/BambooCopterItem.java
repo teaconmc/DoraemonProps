7dfbf1f81d46ca36e84b00d3167024364d5af284
TartaricAcid <baka943@qq.com>
2021-01-02 15:05:47 +0800

    add: bamboo copter item texture and tooltips

--- src/main/java/org/teacon/doraemonprops/item/BambooCopterItem.java
+++ src/main/java/org/teacon/doraemonprops/item/BambooCopterItem.java
@@ -2,6 +2,7 @@ package org.teacon.doraemonprops.item;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,15 +11,21 @@ import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.teacon.doraemonprops.DoraemonProps;
 import org.teacon.doraemonprops.client.model.BambooCopterModel;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class BambooCopterItem extends ArmorItem {
     private static final ResourceLocation TEXTURE = new ResourceLocation(DoraemonProps.MOD_ID, "textures/entity/bamboo_copter.png");
+    private static final double UP_DOWN_SPEED = 0.1;
 
     public BambooCopterItem() {
         super(ArmorMaterial.LEATHER, EquipmentSlotType.HEAD, (new Item.Properties()).group(ItemGroup.TOOLS));
@@ -34,15 +41,15 @@ public class BambooCopterItem extends ArmorItem {
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
         Vector3d motion = player.getMotion();
         if (isJumpKeyDown()) {
-            player.setMotion(motion.x, 0.1, motion.z);
+            player.setMotion(motion.x, UP_DOWN_SPEED, motion.z);
             return;
         }
         if (isSneakKeyDown()) {
-            player.setMotion(motion.x, -0.1, motion.z);
+            player.setMotion(motion.x, -UP_DOWN_SPEED, motion.z);
             return;
         }
         if (!player.isOnGround()) {
-            player.setMotion(motion.x, MathHelper.cos(player.ticksExisted / 5f) / 100f, motion.z);
+            player.setMotion(motion.x, getJitterDistance(player.ticksExisted), motion.z);
         }
         // TODO: 2021/1/1 目前并没有效果，还是无法抑制摔落伤害
         player.fallDistance = 0;
@@ -56,6 +63,10 @@ public class BambooCopterItem extends ArmorItem {
         return Minecraft.getInstance().gameSettings.keyBindSneak.isKeyDown();
     }
 
+    private double getJitterDistance(int ticksExisted) {
+        return MathHelper.cos(ticksExisted / 5f) / 100f;
+    }
+
     @Nullable
     @Override
     @SuppressWarnings("unchecked")
@@ -67,5 +78,12 @@ public class BambooCopterItem extends ArmorItem {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
         return TEXTURE.toString();
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("item.doraemon_props.bamboo_copter.desc"));
     }
 }
